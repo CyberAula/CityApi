4411e2cc7151db7bdc30e91b9e2c0879d8b112e1
Marta Volpini López <marta.volpini@alumnos.upm.es>
2024-04-28 12:41:28 +0200

    js y proyecto base

--- routes/index.js
+++ routes/index.js
@@ -7,19 +7,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'LEGO City' });
 });
 
-//Renderiza la vista clima.ejs
-router.get('/clima', function(req, res, next) {
-  res.render('clima', {title: 'Clima', clima: null}); 
-});
-
-router.post('/clima', async (req, res) => {
-  try {
-      const {fecha } = req.body;
-      const clima = await obtenerClimaPorFecha(fecha);
-      res.render('clima', { title: 'Clima', clima });
-  } catch (error) {
-      res.status(500).send(error.message);
-  }
-});
-
 module.exports = router;
