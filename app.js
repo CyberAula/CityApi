5ba0526e5af5eba3b95f12d3360004ab3cf4a0d3
Marta Volpini López <marta.volpini@alumnos.upm.es>
2024-05-08 18:32:13 +0200

    index

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
